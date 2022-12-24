fef63d6841f24af3cb4ebb90c587001007031971
Avalynn <100826194+avalynndev@users.noreply.github.com>
2022-12-24 11:05:39 +0530

    Update ytmp3.js

--- storage/ytmp3.js
+++ storage/ytmp3.js
@@ -2,4 +2,5 @@ function converter(youtubelink, buttonApi) {
   buttonApi.src =
     "https://yt2mp3.co/api/widget?url=https://www.youtube.com/watch?v=" +
     encodeURIComponent(youtubelink);
+  console.log(buttonApi.src)
 }
