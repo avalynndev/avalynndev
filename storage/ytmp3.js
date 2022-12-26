30987d620ab96dab7d0f14fde52c24adcd9eb9b5
Avalynn <100826194+avalynndev@users.noreply.github.com>
2022-12-26 10:13:26 +0530

    Delete ytmp3.js

--- storage/ytmp3.js
+++
@@ -1,6 +0,0 @@
-function converter(youtubelink, buttonApi) {
-  buttonApi.src =
-    "https://yt2mp3.co/api/widget?url=https://www.youtube.com/watch?v=" +
-    encodeURIComponent(youtubelink);
-  console.log(buttonApi.src)
-}
